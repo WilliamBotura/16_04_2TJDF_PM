5610f50a7fe4d2d7048594be8d3e82f370717e51
William <williamdigitalwork@gmail.com>
2019-05-21 09:44:08 -0300

    21_05
    Criação de script de geração de helicopteros, e rotação aleatoria dos helicopteros

---
+++ Assets/Scripts/geradorHelicopteros.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class geradorHelicopteros : MonoBehaviour
+{
+
+    public GameObject helicopteroPrefab;
+    public float delay = 2f;
+
+	void Start ()
+    {
+        InvokeRepeating("GerarHelicoptero", delay, delay);
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    void GerarHelicoptero()
+    {
+        GameObject helicoptero = Instantiate(helicopteroPrefab);
+
+        int numeroAleatorio = Random.Range(0, 4);
+
+        helicoptero.transform.eulerAngles = Vector3.up * numeroAleatorio * 90;
+    }
+}
